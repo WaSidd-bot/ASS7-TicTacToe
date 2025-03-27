f0547a53f470a6c85dbb27763f38b2440708b9a3
unknown <345654081@TDSB.ON.CA>
2025-03-27 11:38:13 -0400

    boiler code for array testing

---
+++ src/TicTacToe.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) throws Exception {
+        
+        char[] test = {'f'};
+
+// The for-each loop
+
+for (char value : test) {
+
+System.out.println(value);
+
+}
+    }
+}
