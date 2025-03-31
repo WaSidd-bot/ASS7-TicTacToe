74b6f7a629c847d0e522ba1d88c08074a4124182
unknown <345654081@TDSB.ON.CA>
2025-03-31 11:37:42 -0400

    logic for drawing board completed

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,16 +1,31 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args) throws Exception {
+
+    public static void gameField() {
         
-        char[] test = {'f'};
+    }
 
-// The for-each loop
+    public static void main(String[] args) throws Exception {
+        final int GRID_SIZE = 3;
+        Scanner scan = new Scanner(System.in);
 
-for (char value : test) {
+        int row = 0;
+        int col = 0;
 
-System.out.println(value);
+        System.out.println("Hello operator, welcome to TicTacToe!");
+        System.out.println("Please read the rules below and enjoy your game!");
 
-}
+        String[][] drawBoard = new String[3][3];
+
+        for (row = 0; row < drawBoard.length; row++) {
+            for (col = 0; col < drawBoard.length; col++) {
+                drawBoard[row][col] = " ";
+                System.out.print("[" + drawBoard[row][col] + "]");
+            }
+            System.out.println();
+
+        }
+        drawBoard[0][2] = "x";
     }
 }
